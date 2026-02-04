b30e6c906cf07090ca50084c365385d246c959cb
Liam Lethbridge <69471164+MrClever101@users.noreply.github.com>
2026-02-04 01:36:27 +0000

    coloured grid generated, but uniqueness of solutions not guaranteed

--- src/game_logic/queens.rs
+++ src/game_logic/queens.rs
@@ -1,16 +1,18 @@
-use rand::seq::SliceRandom;
-use rand::rng;
+use std::vec;
 
-pub fn generate_grid(grid_size: u32) -> Option<Vec<u32>> {
+use rand::seq::SliceRandom;
+use rand::{Rng, rng};
+
+pub fn generate_grid(grid_size: u32) -> Vec<u32> {
     let mut grid = vec![0; (grid_size * grid_size) as usize];
     if add_row(&mut grid, 0, grid_size) {
-        Some(grid)
+        return grid;
     } else {
-        None
+        return vec![0; (grid_size * grid_size) as usize];
     }
 }
 
-pub fn add_row(grid: &mut Vec<u32>, row: u32, size: u32) -> bool {
+fn add_row(grid: &mut Vec<u32>, row: u32, size: u32) -> bool {
     if row == size {
         return true;
     }
@@ -28,7 +30,7 @@ pub fn add_row(grid: &mut Vec<u32>, row: u32, size: u32) -> bool {
             grid[index as usize] = 0;
         }
     }
-    false
+    return false;
 }
 
 fn is_valid(grid: &Vec<u32>, row: u32, col: u32, size: u32) -> bool {
@@ -50,7 +52,89 @@ fn is_valid(grid: &Vec<u32>, row: u32, col: u32, size: u32) -> bool {
             }
         }
     }
-    true
+    return true;
+}
+
+pub fn create_queens_game(grid_size: u32) -> Vec<u32> {
+    let queens_grid = generate_grid(grid_size);
+
+    if queens_grid.iter().sum::<u32>() == 0 {
+        return queens_grid;
+    }
+    let mut queue: Vec<(u32, u32)> = Vec::new();
+    let mut colour_grid = vec![0; (grid_size * grid_size) as usize];
+    let mut counter = 0;
+
+    // find all queens and give them each a different colour value
+    for row in 0..grid_size {
+        for col in 0..grid_size{
+            if queens_grid[(row*grid_size+col) as usize] == 1 {
+                colour_cell(&colour_grid, &mut queue, row, col, grid_size);
+                counter += 1;
+                colour_grid[(row*grid_size+col) as usize] = counter;
+            }
+        }
+    }
+    
+    while queue.len() >0 {
+        let idx = rng().random_range(0..queue.len());
+        let (row, col) = queue.remove(idx);
+        let colour = find_colours(&colour_grid, row, col, grid_size);
+        // ToDo: check for possible solutions allowed by completing grid
+        // ToDo (maybe): assert that the colour with the smallest frequency is picked rather than random
+        colour_cell(&colour_grid, &mut queue, row, col, grid_size);
+        colour_grid[(row*grid_size+col) as usize] = colour;
+    }
+    
+
+
+    return colour_grid;
 }
 
 
+fn colour_cell(colour_grid: &Vec<u32>, queue: &mut Vec<(u32, u32)>, row: u32, col: u32, size: u32) {
+    let row = row as i32;
+    let col = col as i32;
+    let size = size as i32;
+
+    // check for each directly adjacent cell if it is uncoloured, if so push to queue.
+    for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let r = row + dr;
+        let c = col + dc;
+
+        // careful to check if row or column is out of bounds
+        if r >= 0 && r < size && c >= 0 && c < size {
+            let idx = (r * size + c) as usize;
+            if colour_grid[idx] == 0 {
+                queue.push((r as u32, c as u32));
+            }
+        }
+    }
+}
+
+
+fn find_colours(colour_grid: &Vec<u32>, row: u32, col: u32, size: u32) -> u32{
+    let row = row as i32;
+    let col = col as i32;
+    let size = size as i32;
+    let mut colours: Vec<u32> = Vec::new();
+
+    // check for each directly adjacent cell. If coloured, add it to queue
+    for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let r = row + dr;
+        let c = col + dc;
+
+        if r >= 0 && r < size && c >= 0 && c < size {
+            let idx = (r * size + c) as usize;
+            let c = colour_grid[idx];
+            if c != 0 {
+                colours.push(c);
+            }
+        }
+    }
+    if colours.len() == 0{
+        return 0;
+    }
+    let idx = rng().random_range(0..colours.len());
+    return colours[idx];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 mod game_logic;
-use crate::game_logic::queens::generate_grid;
+use crate::game_logic::queens::create_queens_game;
 
 extern crate rand;
 fn main() {
     let size = 8;
-    match generate_grid(size) {
-        Some(grid) => {
-            for r in 0..size {
-                for c in 0..size {
-                    print!(
-                        "{} ",
-                        grid[(r * size + c) as usize]
-                    );
-                }
-                println!();
-            }
+    let grid = create_queens_game(size);
+    let sum: u32 = grid.iter().sum();
+    if sum == 0{
+        println!("No possible solution");
+        return;
+    }
+    
+
+    for r in 0..size {
+        for c in 0..size {
+            print!(
+                "{} ",
+                grid[(r * size + c) as usize]
+            );
         }
-        None => println!("No possible solution"),
+        println!();
     }
 }
